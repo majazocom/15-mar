58c59b9241fa4027126bc734c9d91f00d044718c
majazocom <maja.gedda@zocom.se>
2021-03-15 10:31:06 +0100

    the three redux concepts added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
 console.log('hej från index.js');
 
 const BUY_CHEESE = 'BUY_CHEESE'; // konstant sträng
+const ADD_CHEESE = 'ADD_CHEESE'; // konstant sträng
 
 function buyCheese() {
     return {
         type: BUY_CHEESE,
         info: 'One less cheese in the store please'
+    }
+}
+
+function addCheese() {
+    return {
+        type: ADD_CHEESE,
+        info: 'One more cheese to the store please'
     }
 }
 
@@ -21,6 +33,20 @@ const reducer = (state = initialState, action) => {
             ...state, //här ber vi funktionen att skapa en kopia av det tidigare statet (vi muterar inte statet - vi returnerar ett helt nytt objekt)
             numberOfCheeses: state.numberOfCheeses - 1  // och sen bara ändra på detta state
         }
+        case ADD_CHEESE: return {
+            ...state,
+            numberOfCheeses: state.numberOfCheeses + 1
+        }
         default: return state  //back-up om vi inte får någon träff på action - så returnerar vi bara det initiala statet - AKA ingen förändring/skada skedd
     }
 }
+
+
+const store = createStore(reducer); //skapa vår store å koppla vår reducer till denne
+
+console.log('initial state ', store.getState()); //logga ut statet
+
+//nästa steg är att meddela vår store att något händer
+store.subscribe(() => {
+    console.log('updated state ', store.getState());
+})
